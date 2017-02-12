cf9162f4805fd499dbc9d207d5cd2e58e7b8c16a
ryuunoakaihitomi <18475860917@163.com>
2017-02-12 08:05:42 +0800

    Add files via upload
    reset upload

--- app/src/main/java/com/ryuunoakaihitomi/QSDcpuLock/serviceNotificationLayout.java
+++ app/src/main/java/com/ryuunoakaihitomi/QSDcpuLock/serviceNotificationLayout.java
@@ -11,7 +11,7 @@ public class serviceNotificationLayout
 		i.setClass(c, MainActivity.class);
 		PendingIntent p = PendingIntent.getActivity(c, 0, i, 0);
 		n.setLatestEventInfo(c, "高通骁龙CPU锁核心方案开机自启", "设置完成", p);
-		n.flags=Notification.FLAG_AUTO_CANCEL;
+		n.flags = Notification.FLAG_AUTO_CANCEL;
 		n.defaults = Notification.DEFAULT_ALL;
 		nm.notify(10, n);
 	}
